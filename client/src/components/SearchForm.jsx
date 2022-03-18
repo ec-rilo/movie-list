50afa100627f5df63b550a208a7ea33d42c61d59
Edgar Carrillo <ecarrillo046@gmail.com>
2022-03-18 12:10:09 -0700

    Refactor SearchForm.jsx to clear input on submit

--- client/src/components/SearchForm.jsx
+++ client/src/components/SearchForm.jsx
@@ -20,8 +20,11 @@ class SearchForm extends React.Component {
       <form className="search-form" onSubmit={(e) => {
         e.preventDefault();
         this.props.searchHandler(this.state.text);
+        this.setState({
+          text: ''
+        });
       }}>
-        <input type='text' placeholder={this.props.btnPlaceholder} onChange={(e) => this.updateText(e.target.value)}/>
+        <input type='text' placeholder={this.props.btnPlaceholder} value={this.state.text} onChange={(e) => this.updateText(e.target.value)}/>
         <button type='submit'>{this.props.btnTxt}</button>
       </form>
     );

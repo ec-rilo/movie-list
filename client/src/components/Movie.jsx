77bb2527f47d6f21b9cd092b3c505997a9b4ba5a
Edgar Carrillo <ecarrillo046@gmail.com>
2022-03-19 09:29:20 -0700

    Refactor watch-btn to be in details block

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -1,17 +1,66 @@
 import React from 'react';
 
-const WatchBtn = ({ watched, watchHandler, movieId }) => {
-  let classname = 'watch-btn ';
-  watched ? classname += 'active' : classname +='inactive';
-
-  return <button className={classname} onClick={() => watchHandler(movieId)}>Watched</button>;
+const WatchBtn = ({ watchHandler, movieId, watched, toggleDetailsBlock }) => {
+  return (
+  <input
+  className='watch-btn'
+  type='radio'
+  onClick={() => {toggleDetailsBlock(); watchHandler(movieId);}}
+  defaultChecked={watched}
+  />);
 }
 
-const Movie = ({ movie, watchHandler }) => (
-  <li className='movie'>
-    <p>{movie.title}</p>
-    {movie.watchedVisible && <WatchBtn movieId={movie.id} watched={movie.watched} watchHandler={watchHandler}/>}
-  </li>
+const DetailsBlock = (props) => (
+  <ul className='movie-card describe-block'>
+    <li><span className='bold-text'>Year:</span> </li>
+    <li><span className='bold-text'>Runtime:</span> </li>
+    <li><span className='bold-text'>Metascore:</span> </li>
+    <li><span className='bold-text'>imdbRating:</span> </li>
+    <li>
+      <span className='bold-text'>Watched:</span>
+      <WatchBtn
+      toggleDetailsBlock={props.toggleDetailsBlock}
+      watchHandler={props.watchHandler}
+      movieId={props.movieId}
+      watched={props.watched}
+      />
+    </li>
+  </ul>
 );
+
+class Movie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      detailsVisible: false
+    };
+
+    this.toggleDetailsBlock = this.toggleDetailsBlock.bind(this);
+  }
+
+  toggleDetailsBlock() {
+    this.setState({ detailsVisible: !this.state.detailsVisible })
+  }
+
+  render() {
+    const { movie, watchHandler } = this.props;
+
+    return (
+      <li>
+        <div className='movie-card'>
+          <p className='movie-title' onClick={() => this.toggleDetailsBlock()}>{movie.title}</p>
+        </div>
+        {this.state.detailsVisible &&
+        <DetailsBlock
+        toggleDetailsBlock={this.toggleDetailsBlock}
+        movieId={movie.id}
+        watched={movie.watched}
+        watchHandler={watchHandler}
+        />}
+      </li>
+    );
+  }
+}
 
 export default Movie;
